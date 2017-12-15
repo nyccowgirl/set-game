166896e445122e376b52102cd2c937daf9638605
vagrant <vagrant@vagrant.vm>
2017-12-15 21:25:34 +0000

    static, templates folders added

---
+++ static/js/is_set.js
@@ -0,0 +1,9 @@
+// This file contains the logic flow for determining whether a group of three cards is a set. 
+// Each card will have four attributes = fill, shape, number, and color. 
+// In order for the group to be a set, all attributes must either be the same or different
+// across the three card. 
+
+// See https://www.setgame.com/sites/default/files/instructions/SET%20INSTRUCTIONS%20-%20ENGLISH.pdf 
+// for details about set. 
+
+
